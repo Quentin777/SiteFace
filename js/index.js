eb82465e0360feabf6a75024596e5e8a8f36c0c0
Ansion Quentin <ansion.quentin@outlook.fr>
2017-02-21 14:05:31 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -19,32 +19,7 @@ elNavShow.addEventListener('click', function(e) {
 */
 
 
-$(document).scroll(function() {
-  var documentScrollTop = $(document).scrollTop();
-  if (documentScrollTop > $('#header').height()) {
-    $('#navbar').addClass('fixedattop');
-    var shiftContent = $('#navbar').height();
-    $('#content').css('margin-top', shiftContent + 'px');
-  }
-  else if ($('#navbar').hasClass('fixedattop')) {
-    $('#navbar').removeClass('fixedattop');
-    $('#content').css('margin-top', '0px');
-  }
-});
 
-var header = document.querySelector("header");
-var stickyNav = document.querySelector("#stickyNav");
-
-// TODO: throttle this function for optimal performance in production
-window.addEventListener('scroll', function(e){
-  var scrollPos = window.pageYOffset || document.documentElement.scrollTop;
-  var stickyLine = header.scrollHeight - stickyNav.scrollHeight;
-  if(scrollPos > stickyLine){
-    stickyNav.classList.add("fixed");
-  }else if(stickyNav.classList.contains('fixed')){
-    stickyNav.classList.remove("fixed");
-  }
-});
 
 
 
